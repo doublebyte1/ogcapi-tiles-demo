2cea86a109a0d33c3f0d855b69c0c9cecac14585
doublebyte1 <info@doublebyte.net>
2024-08-01 11:44:02 +0100

    - updated content with more layers - enabled layers by default - updated icon

--- main.js
+++ main.js
@@ -41,34 +41,21 @@ var map = new Map({
                         'NatGeo_World_Map/MapServer/tile/{z}/{y}/{x}',
                     }),
                   }),
-/*                 new TileLayer({
-                    title: 'Esri Tile Layer',
-                    type: 'base',
-                    visible: true,
-                    source: new XYZ({
-                      attributions:
-                        'Tiles © <a href="https://services.arcgisonline.com/ArcGIS/' +
-                        'rest/services/World_Topo_Map/MapServer">ArcGIS</a>',
-                      url:
-                        'https://server.arcgisonline.com/ArcGIS/rest/services/' +
-                        'World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
-                    }),
-                  })
- */            ]
+            ]
         }),
          new LayerGroup({
             title: 'OGC API - Tiles',
             layers: [
                 new TileLayer({
                     title: '<a href="https://maps.gnosis.earth/ogcapi/collections/blueMarble/">Blue Marble',
-                    visible: false,
+                    visible: true,
                     source: new OGCMapTile({
                       url: 'https://maps.gnosis.earth/ogcapi/collections/blueMarble/map/tiles/WebMercatorQuad',
                     }),
                   }),
                   new VectorTileLayer({
                     title: '<a href="https://maps.gnosis.earth/ogcapi/collections/NaturalEarth:cultural:ne_10m_admin_0_countries/">Natural Earth</a>',
-                    visible: false,
+                    visible: true,
                     source: new OGCVectorTile({
                       url: 'https://maps.gnosis.earth/ogcapi/collections/NaturalEarth:cultural:ne_10m_admin_0_countries/tiles/WebMercatorQuad',
                       format: new MVT(),
@@ -82,14 +69,14 @@ var map = new Map({
                   }),
                   new TileLayer({
                     title: '<a href="https://test.cubewerx.com/cubewerx/cubeserv/demo/ogcapi/Foundation/collections/aerofacp_1m/">Airport Facilities Points</a>',
-                    visible: false,
+                    visible: true,
                     source: new OGCMapTile({
                       url: 'https://test.cubewerx.com/cubewerx/cubeserv/demo/ogcapi/Foundation/collections/aerofacp_1m/styles/default/map/tiles/smerc',
                     }),
                   }),
                   new VectorTileLayer({
                     title: '<a href="https://demo.ldproxy.net/daraa/">Daraa</a>',
-                    visible: false,
+                    visible: true,
                     source: new OGCVectorTile({
                       url: 'https://demo.ldproxy.net/daraa/tiles/WebMercatorQuad/',
                       format: new MVT(),
@@ -99,20 +86,18 @@ var map = new Map({
                       'stroke-color': 'red'
                     },
                   }),
-/*                   new VectorTileLayer({
-                    title: 'Large Lakes',
-                    visible: false,
+                  new VectorTileLayer({
+                    title: '<a href="https://demo.ldproxy.net/vineyards/">Vineyards</a>',
+                    visible: true,
                     source: new OGCVectorTile({
-                      url: 'https://demo.pygeoapi.io/master/collections/lakes/tiles/WebMercatorQuad',
-                      //url: 'https://emotional.byteroad.net/collections/hex350_grid_obesity_1920/tiles/WebMercatorQuad',
+                      url: 'https://demo.ldproxy.net/vineyards/tiles/WebMercatorQuad/',
                       format: new MVT(),
                     }),
                     style: {
                       'stroke-width': 1,
                       'stroke-color': 'red'
                     },
-                  }) */
-
+                  })
             ]
         })
     ],
